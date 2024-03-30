57e8be265e4e1bf5c72979a7bc05d63f9323a2b2
Eve Wright <e_wright5@mail.fhsu.edu>
2024-03-29 20:49:40 -0500

    Added occurred column to database and updated schema. Changed the form in create.html to bootstrap and added form group for date of occurrence (occurred) input. Updated update.html, index.html, and blog.py to include occurred

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -13,7 +13,7 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, occurred, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
@@ -24,6 +24,7 @@ def index():
 @login_required
 def create():
     if request.method == 'POST':
+        occurred = request.form['occurred']
         title = request.form['title']
         body = request.form['body']
         error = None
@@ -36,9 +37,9 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                'INSERT INTO post (occurred, title, body, author_id)'
+                ' VALUES (?, ?, ?, ?)',
+                (occurred, title, body, g.user['id'])
             )
             db.commit()
             return redirect(url_for('blog.index'))
@@ -48,7 +49,7 @@ def create():
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, occurred, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
@@ -69,6 +70,7 @@ def update(id):
     post = get_post(id)
 
     if request.method == 'POST':
+        occurred = request.form['occurred']
         title = request.form['title']
         body = request.form['body']
         error = None
@@ -81,9 +83,9 @@ def update(id):
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?'
+                'UPDATE post SET occurred = ?, title = ?, body = ?'
                 ' WHERE id = ?',
-                (title, body, id)
+                (occurred, title, body, id)
             )
             db.commit()
             return redirect(url_for('blog.index'))
