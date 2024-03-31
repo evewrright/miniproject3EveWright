f22d8c7db439a0446f0842e50d398814b56cb8b9
Eve Wright <e_wright5@mail.fhsu.edu>
2024-03-31 17:18:39 -0500

    Changed title input to select dropdown

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -15,7 +15,7 @@ def index():
     posts = db.execute(
         'SELECT p.id, title, body, created, occurred, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+        ' ORDER BY occurred DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
 
