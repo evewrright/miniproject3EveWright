e0ce14a6138d31d9f1fbbce419a8295158881e8a
Eve Wright <e_wright5@mail.fhsu.edu>
2024-04-02 20:25:26 -0500

    Added some data to insert into student table in schema. Added form-group in create,html for selecting student from existing table student. Added student to my index and create routes. Added student first and last to display for each post in index.html

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -13,8 +13,10 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, topic, body, created, occurred, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
+        'SELECT p.id, topic, body, created, occurred, author_id, student_id, s.first_name, s.last_name, username'
+        ' FROM post p'
+        ' JOIN user u ON p.author_id = u.id'
+        ' JOIN student s ON p.student_id = s.id'
         ' ORDER BY occurred DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
@@ -23,28 +25,33 @@ def index():
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
+    db = get_db()
+    students = db.execute(
+        'SELECT id, first_name, last_name FROM student'
+    ).fetchall()
+
     if request.method == 'POST':
         occurred = request.form['occurred']
-        reason = request.form['topic']
+        topic = request.form['topic']
         body = request.form['body']
+        student_id = request.form['student']
         error = None
 
-        if not reason:
+        if not topic:
             error = 'Topic is required.'
 
         if error is not None:
             flash(error)
         else:
-            db = get_db()
             db.execute(
-                'INSERT INTO post (occurred, topic, body, author_id)'
-                ' VALUES (?, ?, ?, ?)',
-                (occurred, topic, body, g.user['id'])
+                'INSERT INTO post (occurred, topic, body, author_id, student_id)'
+                ' VALUES (?, ?, ?, ?, ?)',
+                (occurred, topic, body, g.user['id'], student_id)
             )
             db.commit()
             return redirect(url_for('blog.index'))
 
-    return render_template('blog/create.html')
+    return render_template('blog/create.html', students=students)
 
 
 def get_post(id, check_author=True):
--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -7,13 +7,26 @@ CREATE TABLE user (
   password TEXT NOT NULL
 );
 
+CREATE TABLE student (
+  id INTEGER PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL
+);
+
 CREATE TABLE post (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   author_id INTEGER NOT NULL,
+  student_id INTEGER NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   occurred DATE NOT NULL,
   topic TEXT NOT NULL,
   body TEXT NOT NULL,
-  FOREIGN KEY (author_id) REFERENCES user (id)
+  FOREIGN KEY (author_id) REFERENCES user (id),
+  FOREIGN KEY (student_id) REFERENCES student (id)
 );
+
+INSERT INTO student (id, first_name, last_name) VALUES (786423, 'Zachary', 'Smith');
+INSERT INTO student (id, first_name, last_name) VALUES (786639, 'Kaden', 'Bloom');
+INSERT INTO student (id, first_name, last_name) VALUES (789622, 'Emily', 'Waltz');
+
 
